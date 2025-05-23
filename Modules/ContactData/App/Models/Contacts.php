744ff4327677d77ecb5a189623fa714875443e42
Astomo Pancoro Putro <tomo@kemang.sg>
2025-05-23 06:49:02 +0000

    analytics note

--- Modules/ContactData/App/Models/Contacts.php
+++ Modules/ContactData/App/Models/Contacts.php
@@ -13,9 +13,22 @@ class Contacts extends Model
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
-    
-    /** relationship  */
+    protected $fillable = ['contact_name', 'phone_no', 'last_message_at', 'created_date', 'created_by', 'updated_date', 'updated_by'];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+    const CREATED_AT = 'created_date';
+    const UPDATED_AT = 'updated_date';
+
+    public function updateLastMessageAt($timestamp = null)
+    {
+        $this->last_message_at = $timestamp ?? now();
+        return $this->save();
+    }
 
     protected static function newFactory()
     {
